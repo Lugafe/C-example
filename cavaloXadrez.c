4104c83fef8c010457ba87175d3d034a5d07e929
Lugafe <lugafe2187@gmail.com>
2024-02-17 18:19:06 -0300

    modify

--- cavaloXadrez.c
+++ cavaloXadrez.c
@@ -1,7 +1,64 @@
 #include <stdio.h>
 
+//funcao para mover o cavalo
+int moveL(int p){
+    switch (p)
+    {
+    case 0:
+        if (ok())
+        {
+            
+        }
+        
+        break;
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    case 5:
+        /* code */
+        break;
+    case 6:
+        /* code */
+        break;
+    case 7:
+        /* code */
+        break;
+    }
+        
+}
+//funcao para ver quais movimentos podem ser feitos
+
+
 int main()
 {
-    
+    int cavalo;    
+    int cont = 0;
+    int tabuleiro[8][8] = {
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    tabuleiro[3][4] = cavalo;
+    do
+    {
+        /* code */
+    } while (cont != 64);
+       
+        
+
     return 0;
 }
